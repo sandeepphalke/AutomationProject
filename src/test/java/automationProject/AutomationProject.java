0c0f25ac22aacf9201c9edc119c78a0e4a62dd34
sandeepphalke <phalkesandeep25@gmail.com>
2022-08-11 17:51:14 +0530

    My First Commit

---
+++ src/test/java/automationProject/AutomationProject.java
@@ -0,0 +1,10 @@
+package automationProject;
+
+public class AutomationProject {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Automation Project");
+		System.out.println("Hello Automation Project");
+	}
+
+}
