a2fad518ad7354c80bc51f05275a64e9d855e39f
sandeepphalke <phalkesandeep25@gmail.com>
2022-08-17 16:13:43 +0530

    test commit

--- src/test/java/automationProject/AutomationProject.java
+++ src/test/java/automationProject/AutomationProject.java
@@ -3,8 +3,8 @@ package automationProject;
 public class AutomationProject {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Automation Project");
-		System.out.println("Hello Automation Project");
+		System.out.println("Hello Automation Project 01");
+		System.out.println("Hello Automation Project 02");
 	}
 
 }
